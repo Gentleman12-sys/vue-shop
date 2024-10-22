a59b9925976ad1b2337a377dd967b528cfe2b12e
KorvoDron <gentlemancoderman@gmail.com>
2024-10-23 02:09:34 +0300

    start proj

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+  <main>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </main>
+
+  <RouterView />
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const text = ref('')
+watch(text, () => {
+    console.log(text.value)
+})
+</script>
+
+
+<template>
+    <main>
+        <input type="text" v-model="text">
+    </main>
+</template>
